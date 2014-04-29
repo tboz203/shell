87ed383154db479c7e93628fbd4ec60d1bae42bb
tboz203 <tboz203@gmail.com>
2014-04-28 19:43:57 -0500

    initial bogus version working

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all: test
 
 test: test.c
 	gcc test.c -o test
+
+clean:
+	-rm test
--- test.c
+++ test.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-    printf("%s\n", "Hello World!");
+    /* printf("%s\n", "Hello World!"); */
+    char array[256];
+    char *pointer = array;
+
+    for (;;) {
+        gets(pointer);
+        system(pointer);
+    }
+
     return 0;
 }
